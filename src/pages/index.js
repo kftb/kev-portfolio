c1e4285a173ba602689fdaf6cab431ba45d8d033
Kevin Bay <kftb@dhcp-10-105-187-37.wireless.northwestern.private>
2020-11-26 10:28:06 -0600

    Changed name and title of webpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || "Kev Bay's Portfolio"}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || "Kev Bay's Portfolio"} />
       </Helmet>
       <App />
     </>
