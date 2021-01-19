ff5c8ad091f6635736d842d4096dc6279a15a8a2
Kevin Bay <kevin.bay@outlook.com>
2021-01-18 22:38:34 -0600

    Added some more mock data

--- src/mock/data.js
+++ src/mock/data.js
@@ -28,7 +28,42 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
+    cat: 'tech',
+    img: 'logoalbstadt.png',
+    title: 'Logo Albstadt: Speech therapy office website',
+    info:
+      'This project was focused to build a SEO-optimized, mobile-friendly webpage for a speech therapy office in Albstadt, Germany.',
+    info2:
+      'The website was planned in Figma and programmed from scratch with Gatsby/React to provide a better learning experience. The page is hosted on Netlify as the page itself will only change rarely.',
+    url: 'https://logoalbstadt.netlify.app',
+    repo: 'https://github.com/kftb/logo-albstadt', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    cat: 'tech',
+    img: 'logoalbstadt.png',
+    title: 'Visualizing a network of classes',
+    info:
+      'This project utilized a dataset of students and which classes they have taken. Visualizing it with a network chart revealed interesting insights into what classes are interesting to students.',
+    info2:
+      'The data was obtained via a Python BeautifulSoup/Selenium scraper and visualized with the NetworkX library',
+    url: 'https://logoalbstadt.netlify.app',
+    repo: 'https://github.com/kftb/logo-albstadt', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    cat: 'pm',
     img: 'project.jpg',
+    title: 'Product Management for Healthcare app',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    cat: 'pm',
+    img: 'kelloggmycourses.png',
     title: '',
     info: '',
     info2: '',
@@ -37,17 +72,9 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
+    cat: 'pm',
+    img: 'rocket.png',
+    title: 'Rocket: Trend aggregation for trading',
     info: '',
     info2: '',
     url: '',
@@ -77,23 +104,23 @@ export const footerData = {
     },
   ],
   colors: [
-  {
-    name: "red",
-    hex: "E07A5F"
-  },
-  {
-    name: "blue",
-    hex: "3D405B"
-  },
-  {
-    name: "green",
-    hex: "81B29A"
-  },
-  {
-    name: "yellow",
-    hex: "F2CC8F"
-  },
-  ]
+    {
+      name: 'red',
+      hex: 'E07A5F',
+    },
+    {
+      name: 'blue',
+      hex: '3D405B',
+    },
+    {
+      name: 'green',
+      hex: '81B29A',
+    },
+    {
+      name: 'yellow',
+      hex: 'F2CC8F',
+    },
+  ],
 };
 
 // Github start/fork buttons
