8803144412f21d631ff3785f58c1c582bf1bb9ca
Kevin Bay <kevin.bay@outlook.com>
2021-01-18 22:37:30 -0600

    Adapted portfolio with new component structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,12 @@ import About from './About/About';
 import Projects from './Projects/Projects';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
+import AboutMe from './AboutMe/AboutMe';
 
 import { PortfolioProvider } from '../context/context';
 
 import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
+import Title from './Title/Title';
 
 function App() {
   const [hero, setHero] = useState({});
@@ -27,7 +29,11 @@ function App() {
   return (
     <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
       <Hero />
-
+      <AboutMe />
+      <Projects category="pm" title="Product Management" />
+      <Projects category="tech" title="Tech & Data" />
+      <Projects category="nerd" title="Nerd & Hobbies" />
+      <Contact />
     </PortfolioProvider>
   );
 }
