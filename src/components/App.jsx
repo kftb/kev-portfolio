a5ba690ccfa4b54686415db15273ff19fbbac308
Kevin Bay <kevin.bay@outlook.com>
2021-01-20 21:39:48 -0600

    Removed About component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Hero from './Hero/Hero';
-import About from './About/About';
 import Projects from './Projects/Projects';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
