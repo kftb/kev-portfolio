76b9ca9e6202d95006cadfb2bef964159bc9ad98
Kevin Bay <kevin.bay@outlook.com>
2021-01-21 19:12:18 -0600

    Added to data structure for content prep

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,4 +1,6 @@
+import { Chip } from '@material-ui/core';
 import { nanoid } from 'nanoid';
+import { graphql, useStaticQuery } from 'gatsby';
 
 // HEAD DATA
 export const headData = {
@@ -17,15 +19,65 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'kev.png',
 };
 
 // PROJECTS DATA
 export const projectsData = [
+  {
+    id: nanoid(),
+    cat: 'pm',
+    img: 'project.jpg',
+    title: 'Product Management for Healthcare app',
+    info:
+      'This project was focused to build a SEO-optimized, mobile-friendly webpage for a speech therapy office in Albstadt, Germany.',
+    info2:
+      'The website was planned in Figma and programmed from scratch with Gatsby/React to provide a better learning experience. The page is hosted on Netlify as the page itself will only change rarely.',
+
+    url: '',
+    tag1: 'Professional',
+    tag2: 'Product Management',
+    tag3: 'Analytics',
+    slug: 'pm/healthcare',
+    repo: '', // if no repo, the button will not show up
+    figma: '',
+  },
+  {
+    id: nanoid(),
+    cat: 'pm',
+    img: 'kelloggmycourses.png',
+    title: 'Kellogg MyCourses: A class selection and planning tool',
+    info:
+      'This project was focused to build a SEO-optimized, mobile-friendly webpage for a speech therapy office in Albstadt, Germany.',
+    info2:
+      'The website was planned in Figma and programmed from scratch with Gatsby/React to provide a better learning experience. The page is hosted on Netlify as the page itself will only change rarely.',
+
+    url: '',
+    tag1: 'Academic',
+    tag2: 'Full PM journey',
+    tag3: 'React',
+    slug: 'pm/kelloggmycourses',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    figma: '',
+  },
+  {
+    id: nanoid(),
+    cat: 'pm',
+    img: 'rocket.png',
+    title: 'Rocket: Trend aggregation for trading',
+    info:
+      'This project was focused to build a SEO-optimized, mobile-friendly webpage for a speech therapy office in Albstadt, Germany.',
+    info2:
+      'The website was planned in Figma and programmed from scratch with Gatsby/React to provide a better learning experience. The page is hosted on Netlify as the page itself will only change rarely.',
+
+    url: '',
+    tag1: 'Academic',
+    tag2: 'User discovery',
+    tag3: 'Prototyping',
+    slug: 'pm/rocket',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    figma: '',
+  },
   {
     id: nanoid(),
     cat: 'tech',
@@ -35,50 +87,31 @@ export const projectsData = [
       'This project was focused to build a SEO-optimized, mobile-friendly webpage for a speech therapy office in Albstadt, Germany.',
     info2:
       'The website was planned in Figma and programmed from scratch with Gatsby/React to provide a better learning experience. The page is hosted on Netlify as the page itself will only change rarely.',
+    slug: 'tech/logoalbstadt',
+    tag1: 'React',
+    tag2: 'Programming',
+    tag3: 'Consulting',
     url: 'https://logoalbstadt.netlify.app',
     repo: 'https://github.com/kftb/logo-albstadt', // if no repo, the button will not show up
+    figma:
+      'https://www.figma.com/proto/igYBO1ccqZnScUg6qMNZBo/Logo-Albstadt?node-id=1%3A2&scaling=min-zoom',
   },
   {
     id: nanoid(),
     cat: 'tech',
     img: 'logoalbstadt.png',
-    title: 'Visualizing a network of classes',
+    title: 'WebQuest: A mini-game to deliver gifts via riddles',
     info:
-      'This project utilized a dataset of students and which classes they have taken. Visualizing it with a network chart revealed interesting insights into what classes are interesting to students.',
+      'This little project was the result of a desire to experiment with Phaser as well as the idea to deliver a birthday gift via riddles embedded into a little mini-game.',
     info2:
-      'The data was obtained via a Python BeautifulSoup/Selenium scraper and visualized with the NetworkX library',
+      'The result of this effort is WebQuest, a horribly hacky foray into Javascript and JS-based games and game design in general.',
+    slug: 'tech/webquest',
+    tag1: 'Leisure',
+    tag2: 'Programming',
+    tag3: 'Game Design',
     url: 'https://logoalbstadt.netlify.app',
-    repo: 'https://github.com/kftb/logo-albstadt', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    cat: 'pm',
-    img: 'project.jpg',
-    title: 'Product Management for Healthcare app',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    cat: 'pm',
-    img: 'kelloggmycourses.png',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    cat: 'pm',
-    img: 'rocket.png',
-    title: 'Rocket: Trend aggregation for trading',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/kftb/webquest', // if no repo, the button will not show up
+    figma: '',
   },
 ];
 
